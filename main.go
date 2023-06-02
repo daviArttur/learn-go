7c4eeccd4736aea25bffd7f9719914da51fd64c7
davi_arttur <daviartur2004@gmail.com>
2023-06-02 12:01:18 -0300

    setup

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	ninja1 "github.com/daviArttur/learn-go/ninja/1"
+)
+
+func main() {
+	// ninja1.Ex1()
+	// ninja1.Ex2()
+	// ninja1.Ex3()
+	// ninja1.Ex4()
+	ninja1.Ex5()
+}
